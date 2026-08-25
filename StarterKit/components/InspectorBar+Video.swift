425994498a428ff206bfa874df75c8f6480c0178
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-25 17:12:45 +0000

    Update starter kit for v1.81.1-rc.0

---
+++ StarterKit/components/InspectorBar+Video.swift
@@ -0,0 +1,44 @@
+import IMGLYEditor
+
+// MARK: - Inspector Bar
+
+extension VideoEditorConfiguration {
+  /// The default inspector bar configuration.
+  static var defaultInspectorBar: InspectorBar.Configuration {
+    InspectorBar.Configuration { builder in
+      builder.items { _ in
+        // Caption-only buttons, kept at the start so a caption selection leads with them.
+        InspectorBar.Buttons.editCaptions()
+        InspectorBar.Buttons.captionStyle()
+
+        InspectorBar.Buttons.replace()
+
+        InspectorBar.Buttons.editText()
+        InspectorBar.Buttons.textPresets()
+        InspectorBar.Buttons.formatText()
+        InspectorBar.Buttons.fillStroke()
+        InspectorBar.Buttons.textBackground()
+        InspectorBar.Buttons.textOnPath()
+        InspectorBar.Buttons.addVoiceoverRecording()
+        InspectorBar.Buttons.volume()
+        InspectorBar.Buttons.clipSpeed()
+        InspectorBar.Buttons.crop()
+
+        InspectorBar.Buttons.adjustments()
+        InspectorBar.Buttons.filter()
+        InspectorBar.Buttons.effect()
+        InspectorBar.Buttons.blur()
+        InspectorBar.Buttons.animation()
+        InspectorBar.Buttons.shape()
+
+        InspectorBar.Buttons.selectGroup()
+        InspectorBar.Buttons.enterGroup()
+
+        InspectorBar.Buttons.layer()
+        InspectorBar.Buttons.split()
+        InspectorBar.Buttons.duplicate()
+        InspectorBar.Buttons.delete()
+      }
+    }
+  }
+}
